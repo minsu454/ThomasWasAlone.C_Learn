83c0dc660d1fc7ca2aa55a107e87427771c167e0
ekrxjvpvj0110 <ekrxjvpvj0110@gmail.com>
2024-11-18 15:10:01 +0900

    refactor: 불필요한 find 삭제

--- ThomasWasAlone.C_Learn/Assets/ThomasWasAlone/Scripts/UI/SceneUI/InGame.cs
+++ ThomasWasAlone.C_Learn/Assets/ThomasWasAlone/Scripts/UI/SceneUI/InGame.cs
@@ -8,16 +8,8 @@ using UnityEngine.UI;
 
 public class InGame : BaseSceneUI
 {
-    public override void Init()
-    {
-        base.Init();
-    }
-    
     public void OnClickPauseButton()
     {
-        PausePopup pausePopup = FindObjectOfType<PausePopup>();
-        if(pausePopup != null) return;
-        
         Managers.UI.CreatePopup<PausePopup>();
     }
 }
