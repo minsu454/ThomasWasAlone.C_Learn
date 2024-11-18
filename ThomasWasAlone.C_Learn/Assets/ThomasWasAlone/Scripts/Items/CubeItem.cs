e55e0dceecbcff228b6403eab3939f4cb5472b4f
geuneda <whdrmsvjzl@naver.com>
2024-11-18 21:41:05 +0900

    feat: 큐브 아이템 충돌 및 처리 로직 구현
    - CubeItem 클래스 추가 및 큐브타입검증 로직 구현
    - CubeType 열거형을 정의하여 종류를 관리
    - 확장가능성을 고려하여 구현

---
+++ ThomasWasAlone.C_Learn/Assets/ThomasWasAlone/Scripts/Items/CubeItem.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CubeItem : MonoBehaviour
+{
+    [SerializeField] private CubeType targetCubeType;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent<BaseCube>(out BaseCube cube)) return;
+
+        bool isCorrectCube = targetCubeType switch
+        {
+            CubeType.BigCube => cube is BigCube,
+            CubeType.SmallCube => cube is SmallCube,
+            CubeType.JumpBoostCube => cube is JumpBoostCube,
+            CubeType.LightCube => cube is LightCube,
+            _ => false
+        };
+        
+        if (isCorrectCube)
+        {
+            // OnCollected(cube);
+            Debug.Log($"{targetCubeType} 아이템 습득");
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnCollected(BaseCube cube)
+    {
+        // 확장 시 자식 클래스에서 이 메서드를 오버라이드
+    }
+} 
---
+++ ThomasWasAlone.C_Learn/Assets/ThomasWasAlone/Scripts/Items/CubeType.cs
@@ -0,0 +1,7 @@
+public enum CubeType
+{
+    BigCube,
+    SmallCube,
+    JumpBoostCube,
+    LightCube
+} 
