79020fdc108cc0a715a937b75c51a4d5066aba0d
minsu454 <kminsu454@gmail.com>
2024-11-19 14:33:04 +0900

    [Update] StageManager

--- ThomasWasAlone.C_Learn/Assets/ThomasWasAlone/Scripts/Managers/Independ/StageManager.cs
+++ ThomasWasAlone.C_Learn/Assets/ThomasWasAlone/Scripts/Managers/Independ/StageManager.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> playerObj = new List<GameObject>();
+
     private void Awake()
     {
-        //Managers.Data.MapName;
+        CreateMap();
+    }
+
+    private void CreateMap()
+    {
+        GameObject prefab = Resources.Load<GameObject>($"Prefabs/Map/SaveMap/SaveMap/{Managers.Data.MapName}");
+        GameObject mapGo = Instantiate(prefab);
+
+        Map map = mapGo.GetComponent<Map>();
+
+        foreach (var obj in map.startObj)
+        {
+            for (int i = 0; i < playerObj.Count; i++)
+            {
+                if (playerObj[i].name == obj.name)
+                {
+                    Instantiate(obj);
+                    break;
+                }
+            }
+
+            obj.GetComponent<MeshCollider>().enabled = false;
+        }
     }
 }
